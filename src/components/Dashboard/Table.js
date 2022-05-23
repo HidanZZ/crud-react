98350a55c2baa838e97aae95fe30f995187e5fb0
HidanZZ <aitdaoudelhoussein@gmail.com>
2022-05-23 10:15:06 +0000

    delete

--- src/components/Dashboard/Table.js
+++ src/components/Dashboard/Table.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
-const Table= ({employees})=>{
+const Table= ({employees,handleDelete})=>{
+    employees.forEach((employee,i)=>{
+        employee.id=i+1
+    })
+    const formater= new Intl.NumberFormat('en-Us',{
+        style:'currency',
+        currency:'USD',
+        minimumFractionDigits:null
+    })
     return (
         <div className="contain-table">
         <table className="striped-table">
@@ -18,6 +26,34 @@ const Table= ({employees})=>{
             </tr>
           </thead>
             <tbody>
+                {employees.length>0? (employees.map((employee,i)=>(
+                    <tr key={employee.id}>
+                        <td>{i+1}</td>
+                        <td>{employee.firstName}</td>
+                        <td>{employee.lastName}</td>
+                        <td>{employee.email}</td>
+                        <td>{formater.format(employee.salary)}</td>
+                        <td>{employee.date}</td>
+                        <td className="text-right">
+                            <button
+                                // onClick={() => handleEdit(employee.id)}
+                                className="button muted-button"
+                            >
+                                Edit
+                            </button>
+                        </td>
+                        <td className="text-left">
+                            <button
+                                onClick={() => handleDelete(employee.id)}
+                                className="button muted-button"
+                            >
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
+                    
+                ))):
+                (<tr colSpan={7}>No employees</tr>)}
             </tbody>
         </table>
         </div>
--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React,{useState} from "react";
 import Table from "./Table"
 import Header from "./Header"
+import {employeesData} from "../../data"
+import Swal from "sweetalert2";
 const Dashboard=()=>{
+
+    const [employees, setEmployees] = useState(employeesData);
+    const handleDelete=(id)=>{
+        Swal.fire(
+            {
+                icon:'warning',
+                title:'are you sure you want to delete this?',
+                text:"you won't be able to revert this!",
+                ShowCancelButton:true,
+                confirmButtonText:'Yes, delete this',
+                cancelButtonText:'No , cancel this'
+
+            }
+        ).then(res=>{
+            if(res.value){
+                const [employee]=employees.filter(x=>x.id===id)
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Deleted!',
+                    text: `${employee.firstName} ${employee.lastName}'s data has been deleted.`,
+                    showConfirmButton: false,
+                    timer: 1500,
+                  });
+                  setEmployees(employees.filter(x=>x.id!==id))
+            }
+        })
+    }
     return (
         <div className="container">
             
@@ -9,7 +38,8 @@ const Dashboard=()=>{
             
               />
               <Table
-                // employees={employees}
+                handleDelete={handleDelete}
+                employees={employees}
                
               />
           
